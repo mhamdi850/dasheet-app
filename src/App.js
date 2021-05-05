6750685e3f9892a767c260d8036522465d28c1ec
mhamdi850 <mhamdi.a@live.fr>
2021-05-05 22:08:39 +0200

    share code on github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import data from './data';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>DA SHEET</h1>
+      {
+        data.countries.map(country => (
+          <div key = {country._id}>
+            <a href={`/country/${country._id}`}>
+              <h2>{country.country}</h2> 
+            </a>
+          </div>
+        ))
+      }
     </div>
+     
+   
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,80 @@
+const data = {
+    countries: [
+        {
+            _id:'1',
+            nature:'net sales',
+            phase:'actual',
+            month:'1',
+            year:'2020',
+            country:'france',
+            amount:3000
+        },
+        {
+            _id:'2',
+            nature:'net sales',
+            phase:'actual',
+            month:'2',
+            year:'2020',
+            country:'france',
+            amount:7666
+        },
+        {
+            _id:'3',
+            nature:'net sales',
+            phase:'actual',
+            month:'1',
+            year:'2020',
+            country:'germany',
+            amount:9000
+        },
+        {
+            _id:'4',
+            nature:'net sales',
+            phase:'actual',
+            month:'2',
+            year:'2020',
+            country:'germany',
+            amount:8777
+        },
+        {
+            _id:'6',
+            nature:'gm',
+            phase:'actual',
+            month:'1',
+            year:'2020',
+            country:'france',
+            amount:3000
+        },
+        {
+            _id:'7',
+            nature:'gm',
+            phase:'actual',
+            month:'2',
+            year:'2020',
+            country:'france',
+            amount:7666
+        },
+        {
+            _id:'8',
+            nature:'gm',
+            phase:'actual',
+            month:'1',
+            year:'2020',
+            country:'germany',
+            amount:9000
+        },
+        {
+            _id:'9',
+            nature:'gm',
+            phase:'actual',
+            month:'2',
+            year:'2020',
+            country:'germany',
+            amount:8777
+        },
+       
+       
+    ]
+}
+
+export default data
